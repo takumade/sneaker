1d1ae661d47298be96bc84f5123ae31d96940cb6
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 21:27:51 +0200

    Add search bar, topic

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,6 +10,57 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Shop Page"),);
+    return Column(
+      children: [
+
+
+            Container(
+              padding: EdgeInsetsDirectional.all(12),
+              margin: EdgeInsets.symmetric(horizontal: 25),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Search", style: TextStyle(color: Colors.grey)),
+                  Icon(Icons.search, color: Colors.grey)
+                ],
+              ),
+            ),
+
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25),
+              child: Text("Everyone flies... some fly longer than others", style: TextStyle(color: Colors.grey[600])),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text('Hot Picks 🔥', style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24
+                  ) ),
+                  Text("See All", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),)
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10,),
+
+            Expanded(child: ListView.builder(
+              itemBuilder: (context, index) {
+              return ShoeTile();
+            }))
+
+
+
+      ],
+    );
   }
 }
