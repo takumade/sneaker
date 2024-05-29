9bf7633bafc61c04bb6da9a07b75abe70b8db290
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 20:42:08 +0200

    Add nav bar

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyBottomNav extends StatelessWidget {
+  const MyBottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
