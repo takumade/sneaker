3cd76d257f61b6866b77b855057041c82441815d
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 21:16:08 +0200

    Add drawer buttns

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -33,6 +33,76 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+            return IconButton(
+              icon: Icon(Icons.menu), 
+              color: Colors.black,
+              onPressed: (){
+                Scaffold.of(context).openDrawer();
+              },);
+          }
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                DrawerHeader(
+              decoration: const BoxDecoration(
+    color: Colors.transparent
+),
+              child: Image.asset("lib/images/nike.png", color: Colors.white,),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Divider(
+                color: Colors.grey[800]
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: ListTile(
+                leading: Icon(Icons.home, color: Colors.white,),
+                title: Text("Home", style: TextStyle(color: Colors.white,),),
+              ),
+            ),
+
+              Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: ListTile(
+                leading: Icon(Icons.info, color: Colors.white,),
+                title: Text("About", style: TextStyle(color: Colors.white,),),
+              ),
+
+ 
+
+            ),
+              ],
+            ),
+
+             Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout_rounded, color: Colors.white,),
+                title: Text("Logout", style: TextStyle(color: Colors.white,),),
+              ),
+
+ 
+
+            ),
+
+
+          ],
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
