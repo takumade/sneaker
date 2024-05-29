02d7dbf0125c25637930aa16907036e13c745687
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 21:39:21 +0200

    Add imagein shoe tile

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:sneaker/models/shoe.dart';
 
 class ShoeTile extends StatelessWidget {
-  const ShoeTile({super.key});
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12)
+      ),
+      child: Column(
+        children: [
+        
+        Image.asset(shoe.imagePath)
+        ],
+      ),
+
+    );
   }
 }
