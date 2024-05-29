7669907156f8b5c0bb0960460bbcac7a1900a746
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 20:36:53 +0200

    Link the page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:sneaker/pages/home_page.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
