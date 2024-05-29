378e0dc5aabdcdd0291776921ebf8a3b95c134c1
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 22:20:39 +0200

    Wrap app with notifierprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sneaker/models/cart.dart';
 import 'package:sneaker/pages/intro_page.dart';
 
 void main() {
@@ -11,9 +13,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(create: (context) => Cart(),
+    builder: (context, child) => const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: IntroPage(),
-    );
+    ));
   }
 }
