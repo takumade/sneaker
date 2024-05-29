63c6d9f2c7512b24adcbdf83fd57de2328c87cc3
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-29 20:47:35 +0200

    Add tabs and styling

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class MyBottomNav extends StatelessWidget {
-  const MyBottomNav({super.key});
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: GNav(
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade100,
+        tabs: [
+          GButton(icon: Icons.home, text: 'Shop'),
+          GButton(icon: Icons.shopping_bag_rounded, text: 'Cart'),
+        ]
+      ),
+    );
   }
 }
