76f330d1cbe548b4590ad5c0e24f8ea44d5c6879
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-31 23:07:27 +0200

    Route to shop

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,6 +3,7 @@ import 'package:sneaker/components/cart_item.dart';
 import 'package:sneaker/models/cart.dart';
 import 'package:provider/provider.dart';
 import 'package:sneaker/models/shoe.dart';
+import 'package:sneaker/pages/shop_page.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -28,7 +29,16 @@ class _CartPageState extends State<CartPage> {
 
             const SizedBox(height: 25,),
 
-            Expanded(child: value.getUserCart().isEmpty ? Text("Your cart is empty") :   ListView.builder(
+            Expanded(child: value.getUserCart().isEmpty ? Center(child: Column(
+              children: [
+                Text("Your cart is empty"),
+
+                ElevatedButton.icon(onPressed: (){
+                    Navigator.push(context, 
+                    MaterialPageRoute(builder:(context) => ShopPage() ));
+                }, icon: Icon(Icons.shopping_bag), label: Text("Add products"))
+              ],
+            )) :   ListView.builder(
               itemCount: value.getUserCart().length,
               itemBuilder:(context, index) {
                 Shoe individualShoe = value.getUserCart()[index];
